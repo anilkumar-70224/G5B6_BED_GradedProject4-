308656adbe46c0ec3a559e95bc882585ab50d6c0
anilkumar-70224 <113817713+anilkumar-70224@users.noreply.github.com>
2023-02-12 23:50:12 +0530

    Delete Employee Management _RestAPIAssignmentSolution-main/Employee Management _RestAPIAssignmentSolution-main/EmployeeManagementRest Api directory

--- Employee Management _RestAPIAssignmentSolution-main/Employee Management _RestAPIAssignmentSolution-main/EmployeeManagementRest Api/src/main/java/com/greatlearning/ems/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greatlearning.ems.entity.Roles;
-
-public interface RoleRepository extends JpaRepository<Roles, Integer> {
-
-}
